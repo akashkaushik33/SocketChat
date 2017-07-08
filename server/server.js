4636b7ee147555a0f2478ec459a9aa113eaea167
akashkaushik33 <akashkaushik33@gmail.com>
2017-07-08 23:34:30 +0530

    Built with angular cli , Final patch (Bugs Removed)
    All of the source code is added included  node.js server code. Follow
    the instruction in readme file for running server.

---
+++ server/server.js
@@ -0,0 +1,19 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+io.on('connection' , (socket) => {
+    console.log('User Connected...');
+
+    socket.on('disconnect' , function() {
+        console.log('User Disconnected');
+    });
+
+    socket.on('add-message' , (message , username) => {
+        io.emit('message' , {type : 'new-message' , text:message , username:username});
+    });
+});
+
+http.listen(8000 , () =>{
+    console.log('Server is up and running on port 8000');;
+})
